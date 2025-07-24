0dd97b042c4e37182cd5e4e0d81fc47128d793eb
ashim0429 <reachashim@gmail.com>
2025-07-24 04:36:47 +0000

    removed dist and changed port

--- extension-dashboard/Server/dist/config/allowedCountries.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ALLOWED_COUNTRIES = void 0;
-exports.ALLOWED_COUNTRIES = {
-    uk: 'UK',
-    us: 'US',
-    canada: 'Canada',
-    australia: 'Australia',
-};
